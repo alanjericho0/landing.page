5d7a1a1277140b81b634fdd23c9a72052d376e0b
alanjericho0 <148729235+alanjericho0@users.noreply.github.com>
2023-11-09 16:29:21 -0800

    Add files via upload

---
+++ vape.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector(".btn"), this.addEventListener("click", function(){
+        document.querySelector("vape").scrollIntoView({behavior:'smooth'});
+    });
+});
